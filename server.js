55fc7972989179e2302b849f08d64f88eee463dc
root <root@srv1425314.hstgr.cloud>
2026-02-25 02:31:56 +0000

    75 Hard tracker with daily tasks, weight log, quotes

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Server error');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(content);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`75 Hard tracker running on port ${PORT}`);
+});
